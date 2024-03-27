1c56052dfad9d259ef3c3a3e922a09fca10791a2
RJ Clay <rj@RJs-MacBook-Pro.lan>
2024-03-27 12:25:29 -0400

    adding test

---
+++ test_operators.py
@@ -0,0 +1,13 @@
+import operation as op 
+
+def test_add():
+    assert op.add(4,5) == 9
+
+def test_subtract():
+    assert op.subtract(25,8) == 17
+
+def test_multiply():
+    assert op.multiply(4,5) == 20
+
+def test_divide():
+    assert op.divide(10,5) == 2    
